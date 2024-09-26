5f667399df3240617130aec2d767c6b51c4eb610
umair-jami <lalaumair92@gmail.com>
2024-09-26 17:41:33 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [city,setCity]=useState('')
+  let [wData,setwData]=useState()
+  let getData=(event)=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a1d246c8ec39df999bd6733d0059d1ee&units=metric`)
+    .then((response)=>response.json())
+    .then((finallRespone)=>{
+      console.log(finallRespone)
+      if(finallRespone.cod==404){
+        console.log(finallRespone.cod)
+        setwData()
+      }else{
+        setwData(finallRespone)
+      }
+    })
+    event.preventDefault()
+    console.log(city)
+    setCity("")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple weather App</h1>
+      <div className="in">
+        <form onSubmit={getData}>
+        <input type="text" value={city} onChange={(e)=>setCity(e.target.value)} />
+        <button>sumbit</button>
+        </form>
+      </div>
+      <div className="weatherBox">
+      {wData
+      ?
+      <>
+      <h2 className="city">{wData.name} {wData.sys.country}</h2>
+      <h2 className="temp">{Math.round( wData.main.temp)}</h2>
+      <img src={`https://openweathermap.org/img/w/${wData.weather[0].icon}.png`} className="wimg"/>
+      <p>{wData.weather[0].description}</p>
+      </>
+      :
+      "No Data found"
+    }
+    </div>
+      
     </div>
   );
 }
